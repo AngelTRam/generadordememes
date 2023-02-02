5932e6a406c403e9cca85d2f825f22ae0cfe1781
Angel Tapia Ramírez <57466486+AngelTRam@users.noreply.github.com>
2023-02-01 20:32:19 -0700

    generadormemes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import html2canvas from 'html2canvas';
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [linea1, setLinea1] = useState('');
+  const [linea2, setLinea2] = useState('');
+  const [imagen, setImagen] = useState('');
+
+  const onChangeLinea1 = function(evento){
+    setLinea1(evento.target.value)
+  }
+  const onChangeLinea2 = function(evento){
+    setLinea2(evento.target.value)
+  }
+
+  const onChangeImagen = function(evento){
+    setImagen(evento.target.value)
+  }
+
+  const exportarImagen = function(){
+    html2canvas(document.querySelector("#save")).then(canvas => {
+      var img = canvas.toDataURL("image/png");
+      var link = document.createElement('a');
+      link.download = "meme.png";
+      link.href = img;
+      link.click();
+    });
+    alert('Guardando imagen.')
+
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Generador de memes</h1>
+        <select onChange={onChangeImagen}>
+          <option> SELECCIONA IMAGEN </option>
+          <option value="perrito">"Esta bien"</option>
+          <option value="chica-incendio">Chica con fuego atras</option>
+        </select>
+        <input onChange={onChangeLinea1} type="text" placeholder='Texto superior'></input>
+        <input onChange={onChangeLinea2} type="text" placeholder='Texto Inferior'></input>
+        <button onClick={exportarImagen}>Exportar</button>
+        <div id='save' className='meme'>
+        <span> {linea1}</span> <br/>
+        <span> {linea2}</span>
+        <img src={'/img/'+imagen+'.jpg'}/>
+      </div>
       </header>
+      
+      
     </div>
   );
 }
